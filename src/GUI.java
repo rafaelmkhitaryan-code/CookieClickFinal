f6cfaabb98e6d654339a5213dd71c70efeb90c2b
RafaelMkhitaryan <rafael.mkhitaryan@ht-la.org>
2026-06-09 11:39:12 -0700

    a

--- src/GUI.java
+++ src/GUI.java
@@ -10,12 +10,21 @@ public class GUI implements ActionListener {
     JLabel label;
     JLabel cost1;
     JLabel cost2;
+    JLabel cost3;
+    JLabel cost4;
+    JLabel cost5;
+    JLabel cost6;
+    JLabel cost7;
+    JLabel cost8;
     JButton cookieButton;
     JButton upgradeButton1;
     JButton upgradeButton2;
     JButton upgradeButton3;
     JButton upgradeButton4;
     JButton upgradeButton5;
+    JButton upgradeButton6;
+    JButton upgradeButton7;
+    JButton upgradeButton8;
     int counter = 0;
     Game game;
     ActionListener guiUpdate;
@@ -29,13 +38,22 @@ public class GUI implements ActionListener {
         fillerPanel = new JPanel();
         label = new JLabel("MUCHO TEXTO");
         cost1 = new JLabel("Upgrade Cost: " + game.upgrade1Cost);
-        cost2 = new JLabel("Upgrade Cost: " + game.upgrade1Cost);
+        cost2 = new JLabel("Upgrade Cost: " + game.upgrade2Cost);
+        cost3 = new JLabel("Upgrade Cost: " + game.upgrade3Cost);
+        cost4 = new JLabel("Upgrade Cost: " + game.upgrade4Cost);
+        cost5 = new JLabel("Upgrade Cost: " + game.upgrade5Cost);
+        cost6 = new JLabel("Upgrade Cost: " + game.upgrade6Cost);
+        cost7 = new JLabel("Upgrade Cost: " + game.upgrade7Cost);
+        cost8 = new JLabel("Upgrade Cost: " + game.upgrade8Cost);
         cookieButton = new JButton("COOKIE");
         upgradeButton1 = new JButton("MUCHO CLICK");
         upgradeButton2 = new JButton("SLOW COOK");
-        upgradeButton3 = new JButton("X2");
-        upgradeButton4 = new JButton("4");
-        upgradeButton5 = new JButton("5");
+        upgradeButton3 = new JButton("25 COOKIES PER CLICK");
+        upgradeButton4 = new JButton("15 COOKIES PER SECOND");
+        upgradeButton5 = new JButton("100 COOKIES PER CLICK");
+        upgradeButton6 = new JButton("1000 COOKIES PER CLICK");
+        upgradeButton7 = new JButton("1500 COOKIES PER SEC");
+        upgradeButton8 = new JButton("10000 COOKIES PER CLICK");
         cookieButton.addActionListener(this);
         upgradeButton1.addActionListener(e -> {
             game.buyUpgrade1();
@@ -43,12 +61,61 @@ public class GUI implements ActionListener {
         upgradeButton2.addActionListener(e -> {
             game.buyUpgrade2();
         });
-            cookieButton.setBackground(Color.RED);
-            upgradeButton1.setBackground(Color.BLUE);
-            upgradeButton2.setBackground(Color.YELLOW);
-            upgradeButton3.setBackground(Color.GREEN);
-            upgradeButton4.setBackground(Color.MAGENTA);
-            upgradeButton5.setBackground(Color.ORANGE);
+        upgradeButton3.addActionListener(e -> {
+        game.buyUpgrade3();
+        });
+        upgradeButton4.addActionListener(e -> {
+            game.buyUpgrade4();
+        });
+        upgradeButton5.addActionListener(e -> {
+            game.buyUpgrade5();
+        });
+        upgradeButton6.addActionListener(e -> {
+            game.buyUpgrade6();
+        });
+        upgradeButton7.addActionListener(e -> {
+            game.buyUpgrade7();
+        });
+        upgradeButton8.addActionListener(e -> {
+            game.buyUpgrade8();
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+            cookieButton.setBackground(new Color(102, 51, 0));
+            upgradeButton1.setBackground(new Color(153, 102, 0));
+            upgradeButton2.setBackground(new Color(102, 51, 0));
+            upgradeButton3.setBackground(new Color(153, 102, 0));
+            upgradeButton4.setBackground(new Color(102, 51, 0));
+            upgradeButton5.setBackground(new Color(153, 102, 0));
+            upgradeButton6.setBackground(new Color(102, 51, 0));
+            upgradeButton7.setBackground(new Color(153, 102, 0));
+            upgradeButton8.setBackground(new Color(102, 51, 0));
+
+        cookieButton.setForeground(new Color(45, 21, 21, 169));
+        upgradeButton1.setForeground(new Color(45, 21, 21, 169));
+        upgradeButton2.setForeground(new Color(45, 21, 21, 169));
+        upgradeButton3.setForeground(new Color(45, 21, 21, 169));
+        upgradeButton4.setForeground(new Color(45, 21, 21, 169));
+        upgradeButton5.setForeground(new Color(45, 21, 21, 169));
+        upgradeButton6.setForeground(new Color(45, 21, 21, 169));
+        upgradeButton7.setForeground(new Color(45, 21, 21, 169));
+        upgradeButton8.setForeground(new Color(45, 21, 21, 169));
+
+
+
+
+
 
         guiUpdate = new ActionListener(){
 
@@ -57,6 +124,12 @@ public class GUI implements ActionListener {
                 label.setText("Cookies: " + game.cookieCounter);
                 cost1.setText("Upgrade Cost: " + game.upgrade1Cost);
                 cost2.setText("Upgrade Cost: " + game.upgrade2Cost);
+                cost3.setText("Upgrade Cost: " + game.upgrade3Cost);
+                cost4.setText("Upgrade Cost: " + game.upgrade4Cost);
+                cost5.setText("Upgrade Cost: " + game.upgrade5Cost);
+                cost6.setText("Upgrade Cost: " + game.upgrade6Cost);
+                cost7.setText("Upgrade Cost: " + game.upgrade7Cost);
+                cost8.setText("Upgrade Cost: " + game.upgrade8Cost);
 
 
             }
@@ -65,8 +138,8 @@ public class GUI implements ActionListener {
         updateTimer.start();
 
 //first need to add the label to the panel
-        panel.setSize(600, 400);
-        panel.setLayout(new GridLayout(3,3));
+        panel.setSize(600, 600);
+        panel.setLayout(new GridLayout(6,3));
         panel.add(cookieButton);
         panel.add(upgradeButton1);
         panel.add(upgradeButton2);
@@ -76,15 +149,24 @@ public class GUI implements ActionListener {
         panel.add(upgradeButton3);
         panel.add(upgradeButton4);
         panel.add(upgradeButton5);
-        panel.setBackground(Color.PINK);
+        panel.add(cost3);
+        panel.add(cost4);
+        panel.add(cost5);
+        panel.add(upgradeButton6);
+        panel.add(upgradeButton7);
+        panel.add(upgradeButton8);
+        panel.add(cost6);
+        panel.add(cost7);
+        panel.add(cost8);
+        panel.setBackground(new Color(195, 126, 67));
 
-        fillerPanel.setSize(700,500);
+        fillerPanel.setSize(700,700);
         fillerPanel.setBackground(Color.DARK_GRAY);
 //next, we add the panel to the frame
         frame.add(panel);
         frame.add(fillerPanel);
 
-        frame.setSize(700,500);
+        frame.setSize(700,700);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
 
--- src/Game.java
+++ src/Game.java
@@ -7,6 +7,12 @@ public class Game {
     private int cookiesPerSecond;
     protected int upgrade1Cost;
     protected int upgrade2Cost;
+    protected int upgrade3Cost;
+    protected int upgrade4Cost;
+    protected int upgrade5Cost;
+    protected int upgrade6Cost;
+    protected int upgrade7Cost;
+    protected int upgrade8Cost;
 
     public Game() {
         this.cookieCounter = 0;
@@ -14,6 +20,14 @@ public class Game {
         this.cookiesPerSecond = 0;
         this.upgrade1Cost = 5;
         this.upgrade2Cost = 25;
+        this.upgrade3Cost = 50;
+        this.upgrade4Cost = 100;
+        this.upgrade5Cost = 500;
+        this.upgrade6Cost = 2500;
+        this.upgrade7Cost = 7500;
+        this.upgrade8Cost = 100000;
+
+
 
     Timer timer = new Timer();
      TimerTask slow = new TimerTask() {
@@ -42,7 +56,7 @@ public class Game {
         if (cookieCounter >= upgrade1Cost) {
         cookieCounter -= upgrade1Cost;
         cookiesPerClick++;
-            upgrade1Cost += cookiesPerClick;
+            upgrade1Cost += cookiesPerClick * 2;
         }
 }
 //write a method to buy another upgrade, called buyUpgrade2
@@ -50,9 +64,60 @@ public void buyUpgrade2() {
     if (cookieCounter >= upgrade2Cost) {
         cookieCounter -= upgrade2Cost;
         cookiesPerSecond++;
-        upgrade2Cost += cookiesPerSecond;
+        upgrade2Cost += cookiesPerSecond * 3;
     }
 }
+
+    public void buyUpgrade3 () {
+        if (cookieCounter >= upgrade3Cost) {
+            cookieCounter -= upgrade3Cost;
+             cookiesPerClick = cookiesPerClick + 25;
+             upgrade3Cost = cookiesPerClick * 4;
+        }
+    }
+
+    public void buyUpgrade4 () {
+        if (cookieCounter >= upgrade4Cost) {
+            cookieCounter -= upgrade4Cost;
+            cookiesPerSecond = cookiesPerSecond + 15;
+            upgrade4Cost = cookiesPerClick * 5;
+        }
+    }
+
+
+    public void buyUpgrade5 () {
+        if (cookieCounter >= upgrade5Cost) {
+            cookieCounter -= upgrade5Cost;
+            cookiesPerClick = cookiesPerClick + 100;
+            upgrade5Cost = cookiesPerClick * 6;
+        }
+    }
+
+    public void buyUpgrade6 () {
+        if (cookieCounter >= upgrade6Cost) {
+            cookieCounter -= upgrade6Cost;
+            cookiesPerClick = cookiesPerClick + 1000;
+            upgrade6Cost = cookiesPerClick * 7;
+        }
+    }
+
+    public void buyUpgrade7 () {
+        if (cookieCounter >= upgrade6Cost) {
+            cookieCounter -= upgrade6Cost;
+            cookiesPerSecond = cookiesPerSecond + 1500;
+            upgrade7Cost = cookiesPerSecond * 8;
+        }
+    }
+    public void buyUpgrade8 () {
+        if (cookieCounter >= upgrade8Cost) {
+            cookieCounter -= upgrade8Cost;
+            cookiesPerClick = cookiesPerClick + 10000;
+            upgrade8Cost = cookiesPerClick * 9;
+        }
+    }
+
+
+
 
 
 }
